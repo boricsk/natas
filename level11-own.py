3ca31aa63beb6da8f838f1f9deb0f5e49bc0aefe
boricsk <borics.krisztian@gmail.com>
2022-11-06 19:51:20 +0100

    level11 fix

--- level11-own.py
+++ level11-own.py
@@ -5,12 +5,6 @@ from urllib import response
 import urllib
 import requests
 import re
-import base64
-
-import json
-
-
-
 
 username = 'natas11'
 password = '1KFqoJXi6hRaPluAmk8ESDW4fSysRoIg'
@@ -25,7 +19,7 @@ print(content)
 
 #A sütik XOR kódolással vannak védve. Nézzük meg a kódot.
 
-url = 'http://%s.natas.labs.overthewire.org/index-source.html' % username
+url = 'http://%s.natas.labs.overthewire.org/' % username
 
 reponse = requests.get(url , auth=(username, password))
 content = reponse.text
@@ -40,93 +34,13 @@ url = 'http://%s.natas.labs.overthewire.org/' % username
 sess = requests.session()
 reponse = sess.get(url , auth=(username, password))
 content = reponse.text
-print(sess.cookies)
+print(sess.cookies['data'])
 
-"""
-Amit tudunk : XOR-al kódolt sütik, amit egy a PHP-ben található loaddata függvény kódol. Van a kódban egy $defaultdata = array( "showpassword"=>"no", "bgcolor"=>"#ffffff");
-változó, ami az alapértéket mutatja. A jelszó akkor fog megjelenni, ha a showpassword = yes.
-PoC
-Mivel a kulcsot nem ismerjük, de van egy kódolt és egy plaintext adatunk ebből meg lehet határozni a kulcsot. Ha ez megvan akkor a megfelelő süti értéket vissza kell
-kódolni és ezeket az adatokat kell beküldeni.
-"""
-
-plainTextData = {"showpassword":"no","bgcolor":"#ffffff"}
-#Ezt JSON formába át kell rakni.
-JSONPlainData = str(json.dumps(dict(plainTextData)))
-#print(JSONPlainData)
-#Most dekódoljuk base64-ből az eredeti sütit
-OriginalCookie = 'MGw7JCQ5OC04PT8jOSpqdmkgJ25nbCorKCEkIzlscm5oKC4qLSgubjY%3D'
-OriginalCookieHTMLDecoded = ''
-for char in OriginalCookie:
-    if char != '%':
-        OriginalCookieHTMLDecoded += char
-    else:
-        OriginalCookieHTMLDecoded += '='
-        break
-
-#Ez lesz az az XOR kódolt sztring, amit a kulcs kereséséhez fogunk használni
-OriginalCookieDecBase64 = str(base64.b64decode(OriginalCookieHTMLDecoded))
+Payload = {"data" : "MGw7JCQ5OC04PT8jOSpqdmk3LT9pYmouLC0nICQ8anZpbS4qLSguKmkz"}
 
 
-"""
-XOR
-plain XOR key = cipher
-plain XOR cipher = key
-azaz JSONPlainData XOR OriginalCookieDecBase64 = key
-
-    //$key = '<censored>';
-    $text = $in;
-    $outText = '';
-
-    // Iterate through each character
-    for($i=0;$i<strlen($text);$i++) {
-    $outText .= $text[$i] ^ $key[$i % strlen($key)];
-    }
-"""
-
-def enc_XOR(input, key) :
-
-    #$key = '<censored>';
-    text = input
-    outText = ''
-    # Iterate through each character
-    i = 0
-    while ( i < len(text) ) :
-        outText += chr(ord(text[i]) ^ ord(key[i % len(key)]))
-        i+=1
-    
-    return outText
-
-def enc_XOR2(input, key) :
-
-    #$key = '<censored>';
-    text = input
-    outText = ''
-    # Iterate through each character
-    i = 0
-    while ( i < len(text) ) :
-        outText += chr((text[i]) ^ (key[i % len(key)]))
-        i+=1
-    
-    return outText
-
-Key1 = 'KNHL'
-# print(Key1) 
-# print(plainTextData)
-print(enc_XOR2(JSONPlainData.encode(),OriginalCookieDecBase64.encode()))
-print(JSONPlainData)
-# InjectedData = '{"showpassword":"yes", "bgcolor":"#ffffff"}'
-# JSONInjectedData = str(simplejson.loads(InjectedData))
-# CookieData = base64.b64encode(enc_XOR(InjectedData,Key1).encode())
-# print(CookieData)
-
-
-
-# Payload = {"data" : "MGw7JCQ5OC04PT8jOSpqdmk3LT9pYmhuKSkrIychOm5xbGsqLSguKi1sNQ=="}
-
-
-# url = 'http://%s.natas.labs.overthewire.org/' % username
-# session = requests.Session()
-# reponse = session.get(url , auth=(username, password),cookies=Payload)
-# content = reponse.text
-# print(content)
+url = 'http://%s.natas.labs.overthewire.org/' % username
+session = requests.Session()
+reponse = session.get(url , auth=(username, password),cookies=Payload)
+content = reponse.text
+print(content)
--- level11.php
+++ level11.php
@@ -19,23 +19,31 @@ function xor_encrypt($in, $key) {
 
 //próbéljuk meg kitalálni a kulcsot, iszen van XOR kódolt adatunk és tudjuk annak
 //minek kell lenni (defaultdata)
+//Amit tudunk : XOR-al kódolt sütik, amit egy a PHP-ben található loaddata függvény kódol. Van a kódban egy $defaultdata = array( "showpassword"=>"no", "bgcolor"=>"#ffffff");
+//változó, ami az alapértéket mutatja. A jelszó akkor fog megjelenni, ha a showpassword = yes.
+//PoC
+//Mivel a kulcsot nem ismerjük, de van egy kódolt és egy plaintext adatunk ebből meg lehet határozni a kulcsot. Ha ez megvan akkor a megfelelő süti értéket vissza kell
+//kódolni és ezeket az adatokat kell beküldeni.
 
 $chiphertext = hex2bin("306c3b242439382d383d3f23392a6a766920276e676c2a2b28212423396c726e68282e2a2d282e6e36");
 $plaintext = json_encode($defaultdata);
+$leaked_key = xor_encrypt($plaintext, $chiphertext);
+echo("A keresett kulcs :")."\n";
+echo($leaked_key)."\n"; 
+//A visszakapott eredményből látszik , hogy a kulcs az KNHL.
 $keyBreaked = 'KNHL';
 
-//echo($plaintext);
-//echo($chiphertext);
-
-//a kulcsnak ebből kell jönni, az ismétlődéseket kell keresni
 //echo(xor_encrypt($plaintext,$chiphertext));
 $gooddata = array( "showpassword"=>"yes", "bgcolor"=>"#ffffff");
 $good_data_plain = json_encode($gooddata);
-echo($good_data_plain);
+echo("A payload :")."\n";
+echo($good_data_plain)."\n";
 $good_cipher = xor_encrypt($good_data_plain, $keyBreaked);
-//echo($good_cipher);
+echo("A payload xorozva:")."\n";
+echo($good_cipher)."\n";
 $cookie_data = base64_encode($good_cipher);
-echo($cookie_data);
+echo("A payload xorozva és base64-el kódolva, ezt kell beküldeni:")."\n";
+echo($cookie_data)."\n";
 
 
 ?>
